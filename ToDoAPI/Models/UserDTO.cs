1d1970f0138f174a2b07acd33d3ecee60ee09035
João Vítor Carvalho <mistingerr@gmail.com>
2021-11-22 18:57:57 -0300

    added-db

--- ToDoAPI/Models/UserDTO.cs
+++ ToDoAPI/Models/UserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace ToDoAPI.Models
 {
     public class UserDTO
     {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]  
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
---
+++ ToDoAPI/Models/UserTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAPI.Models
+{
+    public class UserTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Place { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public bool Done { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Att { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+    }
+}
